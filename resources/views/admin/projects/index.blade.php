4bfcfc7228b95ed4ffc91f2d3b7a3ef89d189e8f
LorezoParis1996 <lorenzoparis86@live.it>
2024-07-26 18:40:28 +0200

    index fatta (2)

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,6 +2,30 @@
 
 @section('content')
 <div class="container">
-  @dump($projects)
+    <table class="table table-hover table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Developer</th>
+            <th scope="col">Release Date</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+
+
+          <tr>
+            <td>{{$project->title}}</td>
+            <td>{{$project->developer}}</td>
+            <td>{{$project->release_date}}</td>
+            <td><a class="btn btn-primary btn-sm">View</a>
+
+            </td>
+          </tr>
+
+          @endforeach
+        </tbody>
+      </table>
 </div>
 @endsection
